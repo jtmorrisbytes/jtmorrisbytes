f89383b2db72d9f8a1e4ac7b95a1194ee2d44091
Jordan Morris <jthecybertinkerer@gmail.com>
2018-10-28 17:25:43 -0500

    added travis.yml

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to the-angular-experiment!');
+    expect(page.getParagraphText()).toEqual('Welcome to www-jtmorrisbytes-com!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'the-angular-experiment'`, () => {
+  it(`should have as title 'www-jtmorrisbytes-com'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('the-angular-experiment');
+    expect(app.title).toEqual('www-jtmorrisbytes-com');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the-angular-experiment!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to www-jtmorrisbytes-com!');
   });
 });
