e10764ace20a76b8889497bd3489689ab6c14893
Jordan Morris <jthecybertinkerer@gmail.com>
2018-11-01 22:03:28 -0500

    tweaking config slightly. added second deploy step
    testing to see if travis allows multiple providers

--- scripts/build-deployment.sh
+++ scripts/build-deployment.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 printf "\nbuilding for production\n\n"
 ng build --prod --extract-css --optimization=true --source-map --delete-output-path --output-path="dist"
+cp dist/index.html dist/404.html
--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -3,3 +3,4 @@
 printf "building for development\n\n"
 
 ng build --verbose --delete-output-path --output-path="dist"
+cp dist/index.html dist/404.html
