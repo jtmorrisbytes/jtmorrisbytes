3b2b968e8122f72a06f490acd5029291356c21ff
Jordan Morris <jthecybertinkerer@gmail.com>
2018-11-20 23:52:56 -0600

    created navigation
    - using css grid! 
    - added new logo
    - added basic links
    - added a dropdown menu with credits to jquery for the formula to calculate the width of the body
    - while debugging the dropdown menu, credits go to the mozilla docs page about closures. thanks guys! who knew returning a function lambada containing a function call would keep a reference to "this" in the scope of a window.addeventlistener() call.

--- src/app/components/nav-main/nav-main.component.ts
+++ src/app/components/nav-main/nav-main.component.ts
@@ -1,10 +1,8 @@
-
-
-
-
 import { Component, OnInit, Injectable, inject } from '@angular/core';
 import {TestService} from '@app/services/test-service.service';
+import * as $ from "jquery";
 import { NavigationProviderService } from '@app/services/navigation/navigation-provider.service'
+import {debounce } from "underscore";
 // testing relative import of directory
 
 
@@ -18,21 +16,66 @@ import { NavigationProviderService } from '@app/services/navigation/navigation-p
 
 
 export class AppNavComponent implements OnInit {
-
+  navLinksSelector:string;
+  /* ENSURE THAT YOU UPDATE largeDevicesWidth according to css breakpoints
+     or navbar menu toggling will break;
+  */
+  largeDevicesWidth = 576;
   navTitle = 'placeholder text';
   appTitle = 'placeholder text';
   navSubtitle: string;
   routes: any;
   constructor(private Test: TestService, private navigationProvider: NavigationProviderService) {
-    
+    this.navLinksSelector = "app-nav-links";
     this.appTitle = 'placeholder-text';
     this.navTitle = 'App name placeholder';
-    // this.routes = routes;
-
-    // this.navTitle = appConfig.title;
-    // this.navSubtitle = appConfig.subtitle;
+    window.addEventListener("resize",
+    debounce((event) => this.removeDisplayAttributeWhenNoLongerMobileWidth(event), 2));
    }
-  public addNavLink(name, path) {
+  calcDocumentWidth() {
+    return Math.max(
+      document.documentElement["clientWidth"],
+      document.body["scrollWidth"],
+      document.documentElement["scrollWidth"],
+      document.body["offsetWidth"],
+      document.documentElement["offsetWidth"]
+    );
+  }
+  isSmallDevice() {
+    return this.calcDocumentWidth() < this.largeDevicesWidth;
+  }
+  isLargeDevice() {
+    return this.calcDocumentWidth() >= this.largeDevicesWidth;
+  }
+  removeDisplayAttributeWhenNoLongerMobileWidth(event) {
+    if(this.isLargeDevice()) {
+      document.getElementById("app-nav-links").style.display = "";
+    }
+    
+  }
+  onDropdownButtonClicked() {
+    // calculation credits to jquery
+    
+    const turnOnGrid = () => {
+          // $(this.navLinksSelector).css("display","grid");
+          
+    }
+    //inital state: display none.
+    // on the start of the animation, set the display property to grid
+    if (this.isSmallDevice()) {
+      if (document.getElementById(this.navLinksSelector).style.display === "none"||
+        document.getElementById(this.navLinksSelector).style.display === "") 
+      {
+          document.getElementById(this.navLinksSelector).style.display = "grid";
+      }
+      else {
+        document.getElementById(this.navLinksSelector).style.display = "none";
+      }
+    }
+
+
+    
+   
 
   }
   ngOnInit() {
