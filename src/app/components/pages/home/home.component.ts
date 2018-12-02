cd464d8a02e173e008a3f28f918dc29a3207baa3
Jordan Morris <jthecybertinkerer@gmail.com>
2018-12-02 14:21:39 -0700

    changed home component title

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -11,10 +11,19 @@ export class HomeComponent implements OnInit, IPage {
   title: string;
   subtitle: string;
   titlebarText: string;
+  authorName = 'Jordan T Morris';
+  authorQualities:Array<string>;
+  websiteName= 'jtmorrisbytes.com';
   constructor() {
     this.path ='home'
-    this.titlebarText = this.title = 'jtmorrisbytes.com';
-    this.subtitle = 'An experiment by Jordan Morris';
+    this.titlebarText='';
+    this.title = this.authorName;
+    this.authorQualities= [
+      'Developer.',
+      'IT Guy.',
+      'Really Cool Dude.'
+    ];
+    this.subtitle ="";
 
   }
 
