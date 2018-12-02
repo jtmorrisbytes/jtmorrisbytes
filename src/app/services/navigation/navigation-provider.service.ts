333d8c93fb494dc2b5eaa95be99bcdcc6486b040
Jordan Morris <jthecybertinkerer@gmail.com>
2018-12-01 19:53:43 -0600

    changed navigation provider service to a stub

--- src/app/services/navigation/navigation-provider.service.ts
+++ src/app/services/navigation/navigation-provider.service.ts
@@ -1,78 +1,12 @@
 import { Injectable } from "@angular/core";
-
-const appPagesRoot = '@app/pages/';
-const appPagesModuleLocation = appPagesRoot + 'app-pages.module';
+import { MySQLService} from "../mysql/my-sql.service";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationProviderService {
-  // rootModule = new AppPagesModule();
-  ItemsToEnumerate;
-  // navigationTree: INavigationModule;
-  constructor() {
-    
-    //this.generateNavLinks();
-    // this.navigationTree = {
-    //   name: "App Navigation root",
-    //   path: "",
-    //   objectReference: this.rootModule,
-    //   children:[
-        
-    //   ]
-    // };
-    //this.generateNavigationTree();
+  constructor(private database: MySQLService) {
     
   }
-  generateNavigationTree(){
-  // console.log("generating navigation tree");
-  // console.log(" root object on following line");
-  // let rootModule = this.navigationTree.objectReference;
-  //console.log( rootModule )
-
-  }
-  searchEnumerationTreeForChildren(children:any, result:[{}] = [{}]){
-    if(children){
-      // console.log("children was defined")
-      // console.log(children);
-      if(children.length > 0){
-        if(children.objectReference){
-          
-        }
-        console.log("recursive search found children");
-        console.log(children[0])
-        
-      }
-      
-    }
-    else{
-      console.log("children was undefined or null")
-      console.log(children)
-    }
-    
-  }
-  generateNavigationComponent(objectReference:any){
-      
-  }
-
-
-  // generateNavLinks(){
-  //   for(let i=0; i < this.components.length; i++){
-  //     let currentComponent = this.components[i];
-  //     let currentComponentInstance = new currentComponent;
-  //     let navigationItem:INavigationItem = 
-  //     {
-  //       path: currentComponentInstance.path,
-  //       component: currentComponent,
-  //       children: [],
-  //       title: currentComponentInstance.title,
-  //       titlebarText: currentComponentInstance.titlebarText
-  //     }
-  //     this.navigationItems.push(navigationItem);
-    
-        
-  //   } 
-    
 }
-
